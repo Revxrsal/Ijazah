4dab0169864158203f4cf879798ced605993fa3d
Ali <kasasbeh@proton.me>
2025-02-18 14:36:27 +0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       <h4 class={"my-4"}>
         الدّورة العلميّة المكثفّة - همم ٣
       </h4>
-      <p class={"my-4 text-red-200 text-3xl font-bold direction-rtl"}>
+      <p class={"my-4 text-red-200 text-3xl font-bold direction-rtl leading-relaxed"}>
     انتهى موعد التسجيل - نعتذكر منكم!
       </p>
       <Button variant={"ghost"} class={"mt-2 mb-2 p-6"} onClick={() => navigate("/forgot-serial")}>
